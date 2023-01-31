2966a7ab2356c2d087b6cabfc29190dc20e7f899
BananaOvf <arseniihrynchak06@gmail.com>
2023-01-31 17:22:02 +0300

    Addition

---
+++ GUI_Calculator/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace GUI_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Num1 = new System.Windows.Forms.TextBox();
+            this.textBox_Num2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_result = new System.Windows.Forms.Button();
+            this.label_sum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_Num1
+            // 
+            this.textBox_Num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Num1.Location = new System.Drawing.Point(36, 58);
+            this.textBox_Num1.Multiline = true;
+            this.textBox_Num1.Name = "textBox_Num1";
+            this.textBox_Num1.Size = new System.Drawing.Size(164, 45);
+            this.textBox_Num1.TabIndex = 0;
+            this.textBox_Num1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_Num2
+            // 
+            this.textBox_Num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Num2.Location = new System.Drawing.Point(268, 58);
+            this.textBox_Num2.Multiline = true;
+            this.textBox_Num2.Name = "textBox_Num2";
+            this.textBox_Num2.Size = new System.Drawing.Size(164, 45);
+            this.textBox_Num2.TabIndex = 1;
+            this.textBox_Num2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(31, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сумма:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(263, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Число 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(31, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Число 1:";
+            // 
+            // button_result
+            // 
+            this.button_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_result.Location = new System.Drawing.Point(108, 241);
+            this.button_result.Name = "button_result";
+            this.button_result.Size = new System.Drawing.Size(268, 55);
+            this.button_result.TabIndex = 5;
+            this.button_result.Text = "Результат";
+            this.button_result.UseVisualStyleBackColor = true;
+            this.button_result.Click += new System.EventHandler(this.button_result_Click);
+            // 
+            // label_sum
+            // 
+            this.label_sum.AutoSize = true;
+            this.label_sum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_sum.Location = new System.Drawing.Point(123, 155);
+            this.label_sum.Name = "label_sum";
+            this.label_sum.Size = new System.Drawing.Size(24, 25);
+            this.label_sum.TabIndex = 6;
+            this.label_sum.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 325);
+            this.Controls.Add(this.label_sum);
+            this.Controls.Add(this.button_result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Num2);
+            this.Controls.Add(this.textBox_Num1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Num1;
+        private System.Windows.Forms.TextBox textBox_Num2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_result;
+        private System.Windows.Forms.Label label_sum;
+    }
+}
+
---
+++ GUI_Calculator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+            
+            int.TryParse(textBox_Num1.Text, out num1);
+            int.TryParse(textBox_Num2.Text, out num2);
+
+            label_sum.Text = (num1 + num2).ToString(); // Сложение
+        }
+    }
+}
